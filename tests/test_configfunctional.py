e05294ebe155ff04fe1ffbf09c790c1cd154b637
Jakub Klinkovský <kuba.klinkovsky@gmail.com>
2013-06-26 19:28:33 +0200

    tests: deduplication (test_no_seasonnumber is not needed, name generation is tested in test_name_generation.py)

--- tests/test_configfunctional.py
+++ tests/test_configfunctional.py
@@ -277,3 +277,23 @@ def test_force_overwrite_default():
     expected_files = ['Scrubs - [01x01] - My First Day.avi', 'scrubs - [01x01].avi']
 
     verify_out_data(out_data, expected_files)
+
+
+@attr("functional")
+def test_not_recursive():
+    """Tests the nested files aren't found when not recursive
+    """
+    input_files = [
+        'Scrubs.s01e01.avi',
+        'nested/subdir/Scrubs.s01e02.avi']
+
+    expected_files = [
+        'Scrubs - [01x01] - My First Day.avi',
+        'nested/subdir/Scrubs.s01e02.avi']
+
+    out_data = run_tvnamer(
+        with_files = input_files,
+        with_flags = ['--not-recursive', '--batch'],
+        run_on_directory = True)
+
+    verify_out_data(out_data, expected_files)
