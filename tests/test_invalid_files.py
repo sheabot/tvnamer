fe5858fa5fdd8b2822391af4fd78dc7716bc3c01
Jakub Klinkovský <kuba.klinkovsky@gmail.com>
2013-06-26 19:17:13 +0200

    tests: deduplication

--- tests/test_invalid_files.py
+++ tests/test_invalid_files.py
@@ -9,27 +9,81 @@ from nose.plugins.attrib import attr
 
 @attr("functional")
 def test_simple_single_file():
-    """Boring example
+    """Files without series name should be skipped, unless --series-name=MySeries is specified
     """
 
     out_data = run_tvnamer(
-        with_files = ['Some File.avi'],
+        with_files = ['S01E02 - Some File.avi'],
         with_flags = ["--batch"])
 
-    expected_files = ['Some File.avi']
+    expected_files = ['S01E02 - Some File.avi']
 
     verify_out_data(out_data, expected_files, expected_returncode = 2)
 
 
 @attr("functional")
-def test_no_series_name():
-    """File without series name should be skipped (unless '--name=MySeries' arg is supplied)
+def test_simple_single_file_with_forced_seriesnames():
+    """Specifying 's01e01.avi' should parse when --series-name=SeriesName arg is specified
     """
 
     out_data = run_tvnamer(
-        with_files = ['s01e01 Some File.avi'],
-        with_flags = ["--batch"])
+        with_files = ['S01E02 - Some File.avi'],
+        with_flags = ["--batch", '--series-name', 'Scrubs'])
 
-    expected_files = ['s01e01 Some File.avi']
+    expected_files = ['Scrubs - [01x02] - My Mentor.avi']
+
+    verify_out_data(out_data, expected_files)
+
+
+@attr("functional")
+def test_name_arg_skips_replacements():
+    """Should not apply input_filename_replacements to --series-name=SeriesName arg value
+    """
+
+    conf = r"""
+    {"batch": true,
+
+    "series_name": "Scrubs",
+
+    "input_filename_replacements": [
+        {"is_regex": true,
+        "match": "Scrubs",
+        "replacement": "Blahblahblah"}
+    ]
+    }
+    """
+
+    out_data = run_tvnamer(
+        with_files = ['S01E02 - Some File.avi'],
+        with_config = conf)
+
+    expected_files = ['Scrubs - [01x02] - My Mentor.avi']
+
+    verify_out_data(out_data, expected_files)
+
+
+@attr("functional")
+def test_replacements_applied_before_series_name():
+    """input_filename_replacements apply to filename, before --series-name=SeriesName takes effect
+    """
+
+    conf = r"""
+    {"batch": true,
+
+    "series_name": "Scrubs",
+
+    "input_filename_replacements": [
+        {"is_regex": true,
+        "match": "S01E02 - ",
+        "replacement": ""}
+    ]
+    }
+    """
+
+    out_data = run_tvnamer(
+        with_files = ['S01E02 - Some File.avi'],
+        with_config = conf)
+
+    expected_files = ['S01E02 - Some File.avi']
 
     verify_out_data(out_data, expected_files, expected_returncode = 2)
